ebd63b7c2a4970ed4ae9d15e81c824eec83c27a6
Reetica <srahul@vt.edu>
2017-03-02 21:13:54 -0700

    Initial Commit

---
+++ app/src/main/java/com/neeru/reetica/soangragitexample/SoangraGitExampleActivity.java
@@ -0,0 +1,13 @@
+package com.neeru.reetica.soangragitexample;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SoangraGitExampleActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_soangra_git_example);
+    }
+}
